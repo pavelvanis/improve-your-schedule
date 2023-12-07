c813e026c249a684234466a0570f74b02bd90215
vanis <pavlik.vanis@email.cz>
2023-12-07 18:52:13 +0100

    Test classes in main

--- improve-your-schedule/Program.cs
+++ improve-your-schedule/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Subject subject = new ("M", "Lecture", "2");
+        Console.WriteLine("Subject\n" + subject);
+        
+        Teacher teacher = new ("Jan Molic");
+        Console.WriteLine("Teacher\n" + teacher);
+        
+        ClassRoom classRoom = new ("A-1", 1);
+        Console.WriteLine("ClassRoom\n" + classRoom);
+        
+        Lesson lesson = new (subject, teacher, classRoom);
+        Console.WriteLine("Lesson\n" + lesson);
+        
     }
 }
