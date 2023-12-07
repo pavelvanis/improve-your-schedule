7bf9f1586ba016c7a410d604ea2830d0587a9154
vanis <pavlik.vanis@email.cz>
2023-12-07 18:51:09 +0100

    Create Class Subject.cs

--- improve-your-schedule/Subject.cs
+++ improve-your-schedule/Subject.cs
@@ -1,3 +1,5 @@
+using improve_your_schedule.lib;
+
 namespace improve_your_schedule;
 
 public class Subject
@@ -6,11 +8,22 @@ public class Subject
     private readonly string _type;
     private readonly string _weaklyHours;
     
-    public Subject(string name, string type, string weaklyHours) 
+    public Subject(string name, string type, string weaklyHours)
     {
-        _name = name;
-        _type = type;
-        _weaklyHours = weaklyHours;
+        _name = Validator.CheckNullOrEmpty(name, "Name cannot be null or empty");
+        _type = Validator.CheckNullOrEmpty(type, "Type cannot be null or empty");
+        _weaklyHours = Validator.CheckNullOrEmpty(weaklyHours, "Weakly hours cannot be null or empty");
     }
+    
+    public string GetName() => _name;
+    public string GetType() => _type;
+    public string GetWeaklyHours() => _weaklyHours;
 
+    public override string ToString()
+    {
+        return "Subject: " + _name + "\n" +
+               "Type: " + _type + "\n" +
+               "Weakly hours: " + _weaklyHours + "\n";
+    }
+    
 }
